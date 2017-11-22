dba284f6b5967bcf6c97c1b9a07cc3f1294cba25
Peng Yu <peng.yu@shopify.com>
2017-11-21 22:03:43 -0500

    add more

--- trainer/model.py
+++ trainer/model.py
@@ -5,88 +5,34 @@ slim = tf.contrib.slim
 
 fea_dim = 128
 
-def model(inputs, labels):
+def model(inputs, labels, feature_dim):
     with slim.arg_scope(vgg.vgg_arg_scope()):
-        outputs, end_points = vgg.vgg_16(inputs)
+        _, end_points = vgg.vgg_16(inputs, spatial_squeeze=False)
         
-        global_feature_one = slim.repeat(end_points['vgg_16/pool5'], 2, slim.conv2d, fea_dim, [5, 5], padding='VALID', 
-                                     weights_initializer=tf.truncated_normal_initializer(stddev=0.01))
+        global_feature_one = slim.repeat(end_points['vgg_16/pool5'], 2, slim.conv2d, feature_dim, 5, padding='VALID')
 
-        global_feature = slim.conv2d(global_feature_one,
-                                         num_outputs=fea_dim,
-                                         keneral_size=[5, 5], 
-                                         padding='VALID',
-                                         scope='global_feature',
-                                         weights_initializer=tf.truncated_normal_initializer(stddev=0.01),
-                                         activation_fn=None
-                                         )
+        global_feature = slim.conv2d(global_feature_one, num_outputs=feature_dim, kernel_size=3, padding='VALID', scope='global_feature', activation_fn=None)
 
-        local_feature_pool5 = slim.conv2d(end_points['vgg_16/pool5'], 
-                                         num_outputs=fea_dim,
-                                         keneral_size=[3, 3], 
-                                         padding='SAME',
-                                         scope='lp5',
-                                         weights_initializer=tf.truncated_normal_initializer(stddev=0.01)
-                                         )
-        local_feature_pool4 = slim.conv2d(end_points['vgg_16/pool4'], 
-                                         num_outputs=fea_dim,
-                                         keneral_size=[3, 3], 
-                                         padding='SAME',
-                                         scope='lp4',
-                                         weights_initializer=tf.truncated_normal_initializer(stddev=0.01)
-                                         )
-        local_feature_pool3 = slim.conv2d(end_points['vgg_16/pool3'], 
-                                         num_outputs=fea_dim,
-                                         keneral_size=[3, 3], 
-                                         padding='SAME',
-                                         scope='lp3',
-                                         weights_initializer=tf.truncated_normal_initializer(stddev=0.01)
-                                         )
-        local_feature_pool2 = slim.conv2d(end_points['vgg_16/pool2'], 
-                                         num_outputs=fea_dim,
-                                         keneral_size=[3, 3], 
-                                         padding='SAME',
-                                         scope='lp2',
-                                         weights_initializer=tf.truncated_normal_initializer(stddev=0.01)
-                                         )
-        local_feature_pool1 = slim.conv2d(end_points['vgg_16/pool1'], 
-                                         num_outputs=fea_dim,
-                                         keneral_size=[3, 3], 
-                                         padding='SAME',
-                                         scope='lp1',
-                                         weights_initializer=tf.truncated_normal_initializer(stddev=0.01)
-                                         )
+        local_feature_pool5 = slim.conv2d(end_points['vgg_16/pool5'], num_outputs=feature_dim, kernel_size=[3, 3], padding='SAME', scope='lp5')
+        local_feature_pool4 = slim.conv2d(end_points['vgg_16/pool4'], num_outputs=feature_dim, kernel_size=[3, 3], padding='SAME', scope='lp4')
+        local_feature_pool3 = slim.conv2d(end_points['vgg_16/pool3'], num_outputs=feature_dim, kernel_size=[3, 3], padding='SAME', scope='lp3')
+        local_feature_pool2 = slim.conv2d(end_points['vgg_16/pool2'], num_outputs=feature_dim, kernel_size=[3, 3], padding='SAME', scope='lp2')
+        local_feature_pool1 = slim.conv2d(end_points['vgg_16/pool1'], num_outputs=feature_dim, kernel_size=[3, 3], padding='SAME', scope='lp1')
+
+        contract_local_feature_pool5 = contrast_layer(local_feature_pool5)
+        contract_local_feature_pool4 = contrast_layer(local_feature_pool4)
+        contract_local_feature_pool3 = contrast_layer(local_feature_pool3)
+        contract_local_feature_pool2 = contrast_layer(local_feature_pool2)
+        contract_local_feature_pool1 = contrast_layer(local_feature_pool1)
+        
+        local_feature_pool5_up = slim.conv2d_transpose(tf.concat([contract_local_feature_pool5, local_feature_pool5], axis=3), feature_dim, kernel_size=5, stride=2)
+        local_feature_pool4_up = slim.conv2d_transpose(tf.concat([contract_local_feature_pool4, local_feature_pool4], axis=3), feature_dim*2, kernel_size=5, stride=2)
+        local_feature_pool3_up = slim.conv2d_transpose(tf.concat([contract_local_feature_pool3, local_feature_pool3], axis=3), feature_dim*3, kernel_size=2, stride=2)
+        local_feature_pool2_up = slim.conv2d_transpose(tf.concat([contract_local_feature_pool2, local_feature_pool2], axis=3), feature_dim*4, kernel_size=5, stride=2)
 
 
+def contrast_layer(feature_map, kernel_size=3):
+    margin = kernel_size/2
+    padded_feature_map = tf.pad(feature_map, [[0, 0], [margin, margin], [margin, margin], [0, 0]], 'SYMMETRIC')
+    return feature_map - slim.avg_pool2d(padded_feature_map, [kernel_size, kernel_size], stride=1, padding='VALID')
 
-def contrast_layer(feature_map, keneral_size):
-    return feature_map - slim.avg_pool_2d(feature_map, [3, 3])
-
-
-        self.Fea_P5_LC = self.Contrast_Layer(self.Fea_P5, 3)
-        self.Fea_P4_LC = self.Contrast_Layer(self.Fea_P4, 3)
-        self.Fea_P3_LC = self.Contrast_Layer(self.Fea_P3, 3)
-        self.Fea_P2_LC = self.Contrast_Layer(self.Fea_P2, 3)
-        self.Fea_P1_LC = self.Contrast_Layer(self.Fea_P1, 3)
-
-        #Deconv Layer
-        self.Fea_P5_Up = tf.nn.relu(self.Deconv_2d(tf.concat([self.Fea_P5, self.Fea_P5_LC], axis=3),
-                                                   [1, 22, 22, fea_dim], 5, 2, name='Fea_P5_Deconv'))
-        self.Fea_P4_Up = tf.nn.relu(self.Deconv_2d(tf.concat([self.Fea_P4, self.Fea_P4_LC, self.Fea_P5_Up], axis=3),
-                                                   [1, 44, 44, fea_dim*2], 5, 2, name='Fea_P4_Deconv'))
-        self.Fea_P3_Up = tf.nn.relu(self.Deconv_2d(tf.concat([self.Fea_P3, self.Fea_P3_LC, self.Fea_P4_Up], axis=3),
-                                                   [1, 88, 88, fea_dim*3], 5, 2, name='Fea_P3_Deconv'))
-        self.Fea_P2_Up = tf.nn.relu(self.Deconv_2d(tf.concat([self.Fea_P2, self.Fea_P2_LC, self.Fea_P3_Up], axis=3),
-                                                   [1, 176, 176, fea_dim*4], 5, 2, name='Fea_P2_Deconv'))
-
-        self.Local_Fea = self.Conv_2d(tf.concat([self.Fea_P1, self.Fea_P1_LC, self.Fea_P2_Up], axis=3),
-                                      [1, 1, fea_dim*6, fea_dim*5], 0.01, padding='VALID', name='Local_Fea')
-        self.Local_Score = self.Conv_2d(self.Local_Fea, [1, 1, fea_dim*5, 2], 0.01, padding='VALID', name='Local_Score')
-
-        self.Global_Score = self.Conv_2d(self.Fea_Global,
-                                         [1, 1, fea_dim, 2], 0.01, padding='VALID', name='Global_Score')
-
-        self.Score = self.Local_Score + self.Global_Score
-        self.Score = tf.reshape(self.Score, [-1,2])
-
-        self.Prob = tf.nn.softmax(self.Score)
