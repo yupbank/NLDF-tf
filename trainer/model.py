2084470ed749e1fb3d3fb275dddd44e5d1426078
Peng Yu <peng.yu@shopify.com>
2017-11-22 17:12:14 -0500

    addd

--- trainer/model.py
+++ trainer/model.py
@@ -3,7 +3,7 @@ from tensorflow.contrib.slim.python.slim.nets import vgg
 
 slim = tf.contrib.slim
 
-fea_dim = 128
+feature_dim = 128
 
 def model(inputs, labels, feature_dim):
     with slim.arg_scope(vgg.vgg_arg_scope()):
@@ -12,6 +12,8 @@ def model(inputs, labels, feature_dim):
         global_feature_one = slim.repeat(end_points['vgg_16/pool5'], 2, slim.conv2d, feature_dim, 5, padding='VALID')
 
         global_feature = slim.conv2d(global_feature_one, num_outputs=feature_dim, kernel_size=3, padding='VALID', scope='global_feature', activation_fn=None)
+        
+        global_score = slim.conv2d(global_feature, num_outputs=2, kernel_size=[1, 1], padding='VALID')
 
         local_feature_pool5 = slim.conv2d(end_points['vgg_16/pool5'], num_outputs=feature_dim, kernel_size=[3, 3], padding='SAME', scope='lp5')
         local_feature_pool4 = slim.conv2d(end_points['vgg_16/pool4'], num_outputs=feature_dim, kernel_size=[3, 3], padding='SAME', scope='lp4')
@@ -26,9 +28,18 @@ def model(inputs, labels, feature_dim):
         contract_local_feature_pool1 = contrast_layer(local_feature_pool1)
         
         local_feature_pool5_up = slim.conv2d_transpose(tf.concat([contract_local_feature_pool5, local_feature_pool5], axis=3), feature_dim, kernel_size=5, stride=2)
-        local_feature_pool4_up = slim.conv2d_transpose(tf.concat([contract_local_feature_pool4, local_feature_pool4], axis=3), feature_dim*2, kernel_size=5, stride=2)
-        local_feature_pool3_up = slim.conv2d_transpose(tf.concat([contract_local_feature_pool3, local_feature_pool3], axis=3), feature_dim*3, kernel_size=2, stride=2)
-        local_feature_pool2_up = slim.conv2d_transpose(tf.concat([contract_local_feature_pool2, local_feature_pool2], axis=3), feature_dim*4, kernel_size=5, stride=2)
+        local_feature_pool4_up = slim.conv2d_transpose(tf.concat([contract_local_feature_pool4, local_feature_pool4, local_feature_pool5_up], axis=3), feature_dim*2, kernel_size=5, stride=2)
+        local_feature_pool3_up = slim.conv2d_transpose(tf.concat([contract_local_feature_pool3, local_feature_pool3, local_feature_pool4_up], axis=3), feature_dim*3, kernel_size=2, stride=2)
+        local_feature_pool2_up = slim.conv2d_transpose(tf.concat([contract_local_feature_pool2, local_feature_pool2, local_feature_pool3_up], axis=3), feature_dim*4, kernel_size=5, stride=2)
+
+        local_feature = slim.conv2d(tf.concat([local_feature_pool1, contract_local_feature_pool1, local_feature_pool2_up], axis=3), num_outputs=feature_dim*5, kernel_size=[1, 1], padding='VALID', scope='lf')
+        
+        local_score = slim.conv2d(local_feature, num_outputs=2, kernel_size=[1, 1], padding='VALID', scope='ls')
+
+        prob = tf.nn.softmax(local_score + global_score)
+
+
+
 
 
 def contrast_layer(feature_map, kernel_size=3):
